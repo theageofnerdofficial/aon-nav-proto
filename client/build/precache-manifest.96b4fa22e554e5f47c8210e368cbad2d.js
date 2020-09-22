ca1620fb7f3713998617de94ac4a3a662b21ee84
John Martin <johnjamesmartin@live.com>
2020-09-22 19:39:09 +0100

    Changed jumbotron4

---
+++ client/build/precache-manifest.96b4fa22e554e5f47c8210e368cbad2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c988e10f840c09617ebf34efb3248a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3511eb1aca6200f8977",
+    "url": "/static/css/main.b833ce78.chunk.css"
+  },
+  {
+    "revision": "d68e835b58fbbcb9ad22",
+    "url": "/static/js/2.55103854.chunk.js"
+  },
+  {
+    "revision": "5834515b12001e137b6234b9acf762e8",
+    "url": "/static/js/2.55103854.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3511eb1aca6200f8977",
+    "url": "/static/js/main.cf504e28.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
